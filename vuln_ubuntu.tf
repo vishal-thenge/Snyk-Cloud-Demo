d12dd4b6192471f1cd05295b1e380278ec58cf10
mike <michaelbraunbass@gmail.com>
2023-03-27 13:03:25 -0600

    Added Build Flag

--- vuln_ubuntu.tf
+++ vuln_ubuntu.tf
@@ -3,8 +3,6 @@ resource "aws_instance" "vuln_vm" {
   availability_zone = var.primary_az
   ami           = var.ubuntu_ami
   instance_type = "t2.micro"
-  key_name = var.key_name
- 
   user_data =  "${file("vuln_bootstrap.sh")}"
   tags = {
    Environment = "DEV"
