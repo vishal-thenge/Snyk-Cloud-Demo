cf08eb8677c53b432d5d503711a4e67ae9997d0e
mike <michaelbraunbass@gmail.com>
2023-03-27 12:06:07 -0600

    Added Build Flag

--- variables.tf
+++ variables.tf
@@ -15,13 +15,13 @@ variable "owner" {
 #AWS Region
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 #AWS AZ
 variable "primary_az" {
   description = "primary AZ"
-  default     = "us-east-1a"
+  default     = "us-east-2a"
 }
 
 #AWS VPC CIDR
