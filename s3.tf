334eedb4154e15c51e8636b9a7b8230b4e828322
mike <michaelbraunbass@gmail.com>
2023-03-28 11:21:32 -0600

    added resources

--- s3.tf
+++ s3.tf
@@ -9,29 +9,6 @@ resource "aws_s3_bucket" "exposedbucket" {
   }
 }
 
-# Policies
-resource "aws_s3_bucket_policy" "open_policy" {
-  bucket = aws_s3_bucket.exposedbucket.id
-  policy = data.aws_iam_policy_document.allow_access.json
-}
-
-data "aws_iam_policy_document" "allow_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-}
-
 resource "aws_s3_bucket_acl" "exposedbucket_acl" {
   bucket = aws_s3_bucket.exposedbucket.id
   acl    = "public-read"
@@ -44,8 +21,3 @@ resource "aws_s3_bucket_public_access_block" "private" {
   restrict_public_buckets = true
 }
 
-resource "aws_cloudtrail" "secure" {
-  name           = "secure"
-  s3_bucket_name = aws_s3_bucket.exposedbucket.id
-  enable_logging = false
-}
