2fc020bd268a99874c1246b57a6be66a0aa8df78
mike <michaelbraunbass@gmail.com>
2023-03-28 09:27:00 -0600

    added resources

--- rds.tf
+++ rds.tf
@@ -9,6 +9,38 @@ resource "aws_db_instance" "default" {
   password             = "SecretPassw0rd"
   parameter_group_name = "default.mysql5.7"
   publicly_accessible = true
-  db_subnet_group_name = "${aws_db_subnet_group.external.id}"
+  db_subnet_group_name = "${aws_db_subnet_group.tfgoof_subnet_group.id}"
   skip_final_snapshot = true
+}
+
+resource "aws_db_subnet_group" "tfgoof_subnet_group" {
+  name       = "${var.victim_company}-subnet-group"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Owner = var.owner
+  }
+}
+
+
+resource "aws_vpc" "tfgoofdbvpc" {
+tags = {
+    Owner = var.owner
+  }
+}
+
+resource "aws_subnet" "external_db" {
+  vpc_id                  = aws_vpc.tfgoofdbvpc.id
+  tags = {
+    Owner = var.owner
+  }
+
+}
+
+resource "aws_subnet" "internal_db" {
+  vpc_id                  = aws_vpc.tfgoofdbvpc.id
+  tags = {
+    Owner = var.owner
+  }
+
 }
